971a8806fcae2acd7cf1b08a212985b6191b75a2
Mahesh R M <maheshrmadiwalar@gmail.com>
2026-03-17 23:24:35 +0530

    Auto update: 2026-03-17 23:24:35

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nammacraft.app',
+  appName: 'NammaCraft',
+  webDir: 'dist'
+};
+
+export default config;
